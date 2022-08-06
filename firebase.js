faaf663994fe9374beeee475f5cb594cbadc37b0
Abdulrahman <rahbalood@gmail.com>
2022-08-06 09:18:27 +0300

    Login page compleated

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJXZqNyM20b1F63gWOc9QiEhrDKLr8LEU",
+    authDomain: "signal-clone-e94a2.firebaseapp.com",
+    projectId: "signal-clone-e94a2",
+    storageBucket: "signal-clone-e94a2.appspot.com",
+    messagingSenderId: "70300650266",
+    appId: "1:70300650266:web:25a3dd24e6f2e81318baa3"
+  };
---
+++ screens/LoginScreen.js
@@ -0,0 +1,62 @@
+import { View, Text, StyleSheet, KeyboardAvoidingView } from "react-native";
+import React, { useState } from "react";
+import { Input, Button, Image } from "@rneui/base";
+import { StatusBar } from "expo-status-bar";
+const LoginScreen = ({navigation}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+
+      <Image
+        source={{
+          uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="Email"
+          value={email}
+          onChangeText={(text) => {
+            setEmail(text);
+          }}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" onPress={()=> navigation.navigate("Register")} />
+      <View style={{height: 100}}></View>
+    </KeyboardAvoidingView>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  inputContainer: {
+    width:300
+  },
+  button:{
+    width:200,
+    marginTop:10
+  }
+});
+
+export default LoginScreen;
